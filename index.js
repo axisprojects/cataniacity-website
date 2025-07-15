e91f5f5368c91ea307441d149e10c3b04bede220
Axis Project <mmoschella@blu.it>
2025-07-15 22:58:00 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const PORT = 3000;
+
+// Serve file statici dalla cartella 'public'
+app.use(express.static(path.join(__dirname, 'public')));
+// Homepage
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+// Regolamento
+app.get('/regolamento', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'regolamento.html'));
+});
+// Termini di Servizio
+app.get('/tos', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'tos.html'));
+});
+// Politica sulla Privacy
+app.get('/informativa-privacy', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'privacy.html'));
+});
+// Politica sui Rimborsi
+app.get('/politica-rimborso', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'rimborsi.html'));
+});
+
+// Avvia il server
+app.listen(PORT, () => {
+  console.log(`Server attivo su http://localhost:${PORT}`);
+});
